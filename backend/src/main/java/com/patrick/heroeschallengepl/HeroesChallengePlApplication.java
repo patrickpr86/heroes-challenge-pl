0a08c0ea89df9c5783a5c0a4ada23dc1597e3417
Patrick <sh4dbr@gmail.com>
2021-11-10 10:29:38 -0300

    Project Created

---
+++ backend/src/main/java/com/patrick/heroeschallengepl/HeroesChallengePlApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.heroeschallengepl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesChallengePlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesChallengePlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/patrick/heroeschallengepl/HeroesChallengePlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.heroeschallengepl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesChallengePlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
